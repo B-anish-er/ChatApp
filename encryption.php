4f884680caabc38eb41b4a67aefd933fea2a6a1f
Shagun Mishra <mshagun2001@gmail.com>
2021-11-22 18:05:16 +0530

    Introduce Encryption

--- encryption.php
+++ encryption.php
@@ -12,8 +12,7 @@ if (!isset($_SESSION['unique_id'])) {
     <section class="chat-area">
       <header>
         <?php
-        // print_r($_GET);
-        $user_id = mysqli_real_escape_string($conn, $_GET['incoming_id']);
+        $user_id = mysqli_real_escape_string($conn, $_GET['user_id']);
         $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$user_id}");
         if (mysqli_num_rows($sql) > 0) {
           $row = mysqli_fetch_assoc($sql);
@@ -28,19 +27,19 @@ if (!isset($_SESSION['unique_id'])) {
           <p><?php echo $row['status']; ?></p>
         </div>
       </header>
-      <div class="chat-box">
 
-      </div>
-      <form action="#" class="typing-area">
+      <!-- <?php echo "chat.php?user_id='" . $_GET['user_id'] . "'"; ?> -->
+      <div class="error-text"></div>
+      <form action="chat.php" class="typing-area">
         <input type="text" class="incoming_id" name="incoming_id" value="<?php echo $user_id; ?>" hidden>
-        <input type="text" name="message" class="input-field" placeholder="Type a message here..." autocomplete="off">
+        <input type="text" name="message" class="input-field" placeholder="Enter Encryption Key here." autocomplete="off">
         <button><i class="fab fa-telegram-plane"></i></button>
       </form>
     </section>
   </div>
 
   <script src="javascript/twofish.js"></script>
-  <script src="javascript/chat.js"></script>
+  <script src="javascript/encryption.js"></script>
 
 </body>
 
